88452af237fba5e5699f7c6d7fb0e214e6f1b69b
2halfone <2halfone@gmail.com>
2025-06-02 21:50:58 +0100

    merge to main

--- chat-service/src/main/java/com/example/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.UUID;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
